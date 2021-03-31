88e50e17f2cad17be4ded028e6bf90167692e2c1
riyakushwaha <reakushwaha@gmail.com>
2021-03-31 14:47:52 +0530

    Mobile version completed

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,16 @@
-const mode-checkbox = document.querySelector(".app__current-theme");
+const mode_checkbox = document.querySelector("#app__current-theme");
+mode_checkbox.addEventListener("change", (event) =>{
+    if (event.currentTarget.checked) {
+        document.querySelector(".app__icon-sun").setAttribute("src", "images/icon-moon.svg")
+        document.documentElement.dataset.theme = "light";
+      } else {
+        document.querySelector(".app__icon-sun").setAttribute("src", "images/icon-sun.svg")
+        document.documentElement.dataset.theme = "dark";
+      }
+});
 
-mode-checkbox
+// Functionality of ToDo Application
+
+const all = [];
+const completed = [];
+const left = [];
